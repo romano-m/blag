3135b51609ef7f0897fb95d380e51972520ff7b6
Romain Monclus <romain.monclus@gmail.com>
2013-12-16 20:00:49 +0100

    web.js creation

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+var k = fs.readFileSync('./index.html');
+
+buf = new Buffer(k);
+
+app.get('/', function(request, response) {
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
